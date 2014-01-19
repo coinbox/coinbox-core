aac588a758f43bf517aba0afbb0455d86035a893
Jad Kik <jadkik94@gmail.com>
2014-01-19 12:58:04 +0200

    Fix installation error: remove argparse from list of packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name="Coinbox-pos",
       version="0.2",
-      packages=find_packages()+['argparse'],
+      packages=find_packages(),
       scripts=['coinbox'],
       
       zip_safe=True,
